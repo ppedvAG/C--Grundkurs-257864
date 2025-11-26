8b78f9caf3be150435c50df4b1bc598c9aeb7a97
dominikenzenhofer <dominike@ppedv.de>
2025-11-26 10:52:19 +0100

    Tag 2 Modul 5

--- Demo-Tag1/Lab03/Program.cs
+++ Demo-Tag1/Lab03/Program.cs
@@ -10,52 +10,73 @@
 		Rechenoperation rechenoperation;
 		bool allesOkay = false;
 
-		Console.Write("Gib eine Zahl ein: ");
-		allesOkay = int.TryParse(Console.ReadLine(), out zahl1);
-		if (allesOkay)
+		do
 		{
+			Console.Write("Gib eine Zahl ein: ");
+			allesOkay = int.TryParse(Console.ReadLine(), out zahl1);
+			if (!allesOkay)
+			{
+				Console.WriteLine("Ungültige Eingabe.");
+				allesOkay=true;
+				continue;
+			}
+			
 			Console.Write("Gib eine weitere Zahl ein: ");
 			allesOkay = int.TryParse(Console.ReadLine(), out zahl2);
-			if(allesOkay)
+			if (!allesOkay)
 			{
-				Console.WriteLine("\n" +
-					"Wähle eine Rechenoperation aus: ");
-				for(int i = 1; i <=4; i++)
-				{
-					Console.WriteLine($"{i}: {(Rechenoperation)i}");
-				}
-				int auswahl;
-				allesOkay = int.TryParse(Console.ReadLine(), out auswahl);
-				if(allesOkay && auswahl >=1 && auswahl <=4)
-				{
-					rechenoperation = (Rechenoperation)auswahl;
-					switch(rechenoperation)
-					{
-						case Rechenoperation.Addition:
-							Console.WriteLine("Ergebnis: " + (zahl1 + zahl2));
-							break;
-						case Rechenoperation.Subtraktion:
-							Console.WriteLine("Ergebnis: " + (zahl1 - zahl2));
-							break;
-						case Rechenoperation.Multiplikation:
-							Console.WriteLine("Ergebnis: " + (zahl1 * zahl2));
-							break;
-						case Rechenoperation.Division:
-							if (zahl2 == 0)
-							{
-								Console.WriteLine("Division dur 0 nicht erlaubt");
-								break;
-							} else
-							{
-								Console.WriteLine("Ergebnis: " + (zahl1 / zahl2));
-								break;
-							}
-
-					}
-				}
-
+				Console.WriteLine("Ungültige Eingabe.");
+				allesOkay=true;
+				continue;
 			}
-		}
+
+			// Auswahl der Rechenoperation (Ausgabe aller Möglichkeiten)
+			Console.WriteLine("\n" +
+				"Wähle eine Rechenoperation aus: ");
+			for (int i = 1; i <= 4; i++)
+			{
+				Console.WriteLine($"{i}: {(Rechenoperation)i}");
+			}
+			int auswahl;
+			allesOkay = int.TryParse(Console.ReadLine(), out auswahl);
+			if (!(allesOkay && auswahl >= 1 && auswahl <= 4))
+			{
+				Console.WriteLine("Ungültige Auswahl. Bitte eine Zahl zwischen 1 und 4 eingeben.");
+				allesOkay = true;
+				continue;
+			}
+
+			rechenoperation = (Rechenoperation)auswahl;
+			switch (rechenoperation)
+			{
+				case Rechenoperation.Addition:
+					Console.WriteLine("Ergebnis: " + (zahl1 + zahl2));
+					break;
+				case Rechenoperation.Subtraktion:
+					Console.WriteLine("Ergebnis: " + (zahl1 - zahl2));
+					break;
+				case Rechenoperation.Multiplikation:
+					Console.WriteLine("Ergebnis: " + (zahl1 * zahl2));
+					break;
+				case Rechenoperation.Division:
+					if (zahl2 == 0)
+					{
+						Console.WriteLine("Division dur 0 nicht erlaubt");
+						break;
+					}
+					else
+					{
+						Console.WriteLine("Ergebnis: " + (zahl1 / zahl2));
+						break;
+					}
+			}
+			
+			Console.WriteLine("Möchten Sie eine weitere Rechnung durchführen? (J/N)");
+			allesOkay = Console.ReadKey().Key == ConsoleKey.J;
+			Console.WriteLine();
+
+
+		} while (allesOkay);
 
 	}
 }
---
+++ DemoTag2/DemoTag2/Program.cs
@@ -0,0 +1,41 @@
+﻿public class Program
+{
+	public static void Main()
+	{
+		// Erste Methode mit out-Parameter
+		//int x;
+		//string ausgabeT = "Bitte eine Zahl eingeben: ";
+		//bool hatFunktioniert = ZahlEinlesen(ausgabeT, out x);
+		//Console.WriteLine($"Die eingegebene Zahl ist: {x} und der Text ist \n{ausgabeT}");
+
+		//bool ZahlEinlesen( string ausgabeText, out int zahl)
+		//{
+		//	Console.WriteLine(ausgabeText);
+		//	bool erfolg = int.TryParse(Console.ReadLine(), out zahl);
+		//	return erfolg;
+		//}
+
+		// Überladene Methoden
+		Math.Round(2.555);
+		Math.Round(2.555, 2);
+
+		bool erfolg = ZahlEinlesen(out int zahl1);
+		erfolg = ZahlEinlesen(out zahl1, "Bitte Zahl eingeben");
+
+	}
+	static private bool ZahlEinlesen(out int zahl)
+	{
+		Console.WriteLine("Zahl eingeben.");
+		bool erfolg = int.TryParse(Console.ReadLine(), out zahl);
+		return erfolg;
+	}
+
+	static private bool ZahlEinlesen(out int zahl, string ausgabeText)
+	{
+		Console.WriteLine(ausgabeText);
+		bool erfolg = int.TryParse(Console.ReadLine(), out zahl);
+		return erfolg;
+	}
+}
+
+
